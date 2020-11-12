60ab62e71b87673843986ebccc15ff0af248a435
petrkudrnovsky <kudrnovsky.p@gmail.com>
2020-11-12 17:51:17 +0100

    new version - new styles, improved (html chars forbidden)

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Online chat</title>
-    </head>
-    <body>
-        <h2>Petrovo diskuzní fórum</h2>
-        <?php
+<?php
         function nactiTridu($trida) {
             require("tridy/$trida.php");
         }
@@ -14,9 +6,34 @@
         
         $chat_db = Databaze::pripoj("localhost", "root", "", "online_chat_db");
         
+?>
+
+<!DOCTYPE html>
+<html lang="cs-cz">
+    <head>
+        <meta charset="UTF-8">
+        <title>Online chat</title>
+        <link href="styles.scss" rel="stylesheet">
+    </head>
+    <body>
+        
+        <div class="header">
+            <h2>Petrovo diskuzní fórum</h2>
+        </div>
+       
+        <form method="post">
+            Přezdívka:<br>
+            <input type="text" name="nickname"><br>
+            Zpráva:<br>
+            <textarea name="message" rows="6" cols="50"></textarea><br>
+            <input type="submit" value="Odeslat" class="btn">
+        </form>
+        
+        <?php
+        
         if($_POST) {
-            $message = $_POST["message"];
-            $nickname = $_POST["nickname"];
+            $message = htmlspecialchars($_POST["message"]);
+            $nickname = htmlspecialchars($_POST["nickname"]);
             ChatHandler::saveUser($nickname, $message);
         };
         
@@ -28,19 +45,17 @@
             echo("<td rowspan='3'><img src='obr/user_icon.png' alt='' width='100px'></td>");
             echo("<td>uživatel: <strong>$uzivatel[1]</strong></td></tr>");
             echo("<tr><td>zpráva: $uzivatel[2]</td></tr>");
-            echo("<tr><td>čas:" . date("d/m/Y h:i:s", $uzivatel[3]) . "</td></tr>");
+            echo("<tr><td class='time'>" . date("d/m/Y H:i:s", $uzivatel[3]) . "</td></tr>");
             echo("<tr><td height='30px'></td></tr>");
         }
         echo("</table>");
         
         
         ?>
-        <form method="post" style="border: 3px solid black; padding: 20px;">
-            Přezdívka:<br>
-            <input type="text" name="nickname"><br>
-            Zpráva:<br>
-            <textarea name="message"></textarea><br>
-            <input type="submit" value="Odeslat">
-        </form>
+        <p class="konec">Zde forum končí - načítá se pouze posledních 10 příspěvků</p>
+        <hr>
+        <footer>
+            <p>Vytvořil <strong>Petr Kudrnovský</strong>, Online Discussion Forum 0.2.1 2020 &copy; All right reserved</p>
+        </footer>
     </body>
 </html>
